b1da983f54d20ed011ade1b003893e0c4750d406
Mimi <mimitraugott@gmail.com>
2019-10-14 16:34:51 -0600

    millionth push, still not getting it without looking

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -4,26 +4,25 @@ import './App.css'
 
 class App extends Component {
   constructor () {
-    super ();
+    super();
 
     this.state = {
       message: ''
-    };
+    }
   }
-
-  handleChange(value) {
+  handleChange (value) {
     this.setState({ message: value });
   }
 
   render () {
     return (
-      <div className = "App">
-        <input onChange={e => this.handleChange(e.target.value)} type='text'/>
+      <div className='App'>
+        <input onChange={e => this.handleChange(e.target.value)} type='text' />
         <p> {this.state.message}</p>
-
       </div>
     )
-    }
+  }
 }
-
+ 
 export default App
+
