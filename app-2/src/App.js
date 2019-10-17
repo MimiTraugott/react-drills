804e386ebaaa1ed48d2a1725b1f13308bd59eca0
Mimi <mimitraugott@gmail.com>
2019-10-16 22:20:43 -0600

    app 2 practice

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,21 +1,19 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
 
 class App extends Component {
   constructor () {
     super();
 
     this.state = {
-      partyItems: ["plates", "bowls", "cups", "ice", "utensils"]
-    };
+    drinks: ["Soda", "Milk", "Beer", "Wine", "Juice", "Tea" ]
   }
-
-  render() {
-    let partyItemsToList = this.state.partyItems.map((element, index) => {return <h2 key={index}>{element}</h2>;
-  });
-
-  return <div className="App">{partyItemsToList}</div>;
+  }
+  render () {
+    let drinksToDisplay=this.state.drinks.map((element, index) => {
+      return <h1 key={index}>{element}</h1>;
+    });
+    return <div className="App">{drinksToDisplay}</div>
   }
 }
-
 export default App;
